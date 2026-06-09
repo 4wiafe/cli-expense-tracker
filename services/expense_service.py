76a1ec9d992b98c3c0863035f759b7f51d193f8d
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-06-09 15:57:41 +0000

    Add ExpenseService class

---
+++ services/expense_service.py
@@ -0,0 +1,29 @@
+from models.expense import Expense
+
+
+class ExpenseService:
+    def __init__(self, storage):
+        self.storage = storage
+        self.expenses = self.storage.load()
+
+    def add_expense(self, date, category, description, amount):
+        expense = Expense(date, category, description, amount)
+        self.expenses.append(expense)
+        self.storage.save(self.expenses)
+
+        return expense
+
+    def list_expenses(self):
+        return self.expenses
+
+    def find_by_id(self, expense_id):
+        for exp in self.expenses:
+            if exp.id == expense_id:
+                return exp
+
+        return None
+
+    def delete_expense(self, expense_id):
+        self.expenses = [exp for exp in self.expenses if exp.id != expense_id]
+
+        return self.expenses
