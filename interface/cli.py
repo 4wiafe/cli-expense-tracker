0990e17659e550bdea8e000e90cb8774a8e123e6
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-06-16 11:37:03 +0000

    Add view summary/analytics to options

--- interface/cli.py
+++ interface/cli.py
@@ -1,5 +1,6 @@
 from services.expense_service import ExpenseService
 from storage.json_storage import JsonStorage
+from reports.reports import Reports
 from utils.validators import to_cents
 
 FILE_PATH = "data/expenses.json"
@@ -8,14 +9,16 @@ FILE_PATH = "data/expenses.json"
 def run_cli():
     storage = JsonStorage(FILE_PATH)
     service = ExpenseService(storage)
+    reports = Reports(storage)
     is_running = True
 
     while is_running:
         print("\n=== EXPENSE TRACKER ===")
         print("1. Add expense")
         print("2. View expenses")
-        print("3. Delete expense")
-        print("4. Exit")
+        print("3. View summary")
+        print("4. Delete expense")
+        print("5. Exit")
 
         choice = input("Select an option: ")
 
@@ -29,6 +32,7 @@ def run_cli():
             print("===========Expense added============")
             print(f"Added expense with id: {expense.id}")
             print("====================================")
+
         elif choice == "2":
             print("=============== All Expenses ===============")
 
@@ -43,10 +47,37 @@ def run_cli():
                 print("======================================")
 
         elif choice == "3":
+            print("1. View total expenses")
+            print("2. View total by category")
+            print("3. View highest expense")
+            print("4. View Lowest expense")
+
+            summary_choice = input("Select an option: ")
+
+            if summary_choice == "1":
+                total_expenses = reports.total_expenses()
+                print(f"Total expenses: {total_expenses / 100:.2f}")
+
+            if summary_choice == "2":
+                category = input("Enter a category: ")
+                total_expense_by_category = reports.total_by_category(category)
+                print(total_expense_by_category)
+
+            if summary_choice == "3":
+                highest_expense = reports.highest_expense_category()
+                print(highest_expense)
+
+            if summary_choice == "4":
+                lowest_expense = reports.lowest_expense_cateory()
+                print(lowest_expense)
+
+        elif choice == "4":
             expense_id = input("Enter id: ")
             service.delete_expense(expense_id)
             print("Expense deleted!")
-        elif choice == "4":
+
+        elif choice == "5":
             is_running = False
+
         else:
             print("Invalid option, please try again")
