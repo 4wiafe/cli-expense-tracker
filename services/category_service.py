16d8ae18b56eca2d2e9b46f099c78bce18ca26f4
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-08-05 18:29:13 +0000

    Refactor and fix minor bugs

--- services/category_service.py
+++ services/category_service.py
@@ -1,19 +1,24 @@
 class CategoryService:
-    def __init__(self, storage) -> None:
-        self.storage = storage.get_categories()
+    def __init__(self, storage):
+        self.storage = storage
+        self.categories = self.storage.get_all_categories()
+
         self.id_to_name = {}
         self.name_to_id = {}
 
-    def _build_id_lookup(self) -> None:
-        for item in self.storage:
-            self.id_to_name[f"{item[0]}"] = item[1]
+        self._build_id_lookup()
+        self._build_name_lookup()
 
-    def _build_name_lookup(self) -> None:
-        for item in self.storage:
-            self.id_to_name[item[1]] = item[0]
+    def _build_id_lookup(self):
+        for category_id, category_name in self.categories:
+            self.id_to_name[category_id] = category_name
+
+    def _build_name_lookup(self):
+        for category_id, category_name in self.categories:
+            self.name_to_id[category_name] = category_id
 
     def get_category_name(self, category_id: int) -> str:
-        return self.id_to_name[str(category_id)]
+        return self.id_to_name[category_id]
 
     def get_category_id(self, category_name: str) -> int:
         return self.name_to_id[category_name]
