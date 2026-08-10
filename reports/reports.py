ec50999e9dc9e78958783a155cd562ee6a41d62a
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-08-10 22:10:03 +0000

    Refactor and expand reports module

--- reports/reports.py
+++ reports/reports.py
@@ -1,40 +1,99 @@
-from datetime import datetime, date
+from storage.expense_storage import ExpenseStorage
+from storage.budget_storage import BudgetStorage
+from services.category_service import CategoryService
 
 
 class Reports:
-    def __init__(self, storage):
-        self.storage = storage
+    def __init__(
+        self,
+        expense_storage: ExpenseStorage,
+        budget_storage: BudgetStorage,
+        category_service: CategoryService,
+    ):
+        self.expense_storage = expense_storage
+        self.budget_storage = budget_storage
+        self.category_service = category_service
 
-    def total_expenses(self) -> int:
-        return self.storage.get_total_expenses()
+    def get_total_expenses(self, user_id: int) -> int:
+        total = self.expense_storage.get_total_expenses(user_id)
 
-    def total_by_category(self, category) -> dict[str, int]:
-        normalized_category = category.strip().capitalize()
-        category_name, total = self.storage.get_spending_by_category(
-            normalized_category
+        return int(f"{total / 100:.2f}")
+
+    def get_spending_by_category(self, user_id: int, category_name: str) -> dict:
+        category_id = self.category_service.get_category_id(category_name)
+
+        fetched_category_id, total = self.expense_storage.get_spending_by_category(
+            user_id, category_id
         )
 
-        return {"category": category_name, "total": total}
+        return {
+            "category_name": self.category_service.get_category_name(
+                fetched_category_id
+            ),
+            "total": f"{total / 100:.2f}",
+        }
 
-    def highest_spending_category(self) -> dict[str, int | str]:
-        highest_category = self.storage.get_highest_spending_category()
-
-        return {highest_category[0]: f"{highest_category[1] / 100:.2f}"}
-
-    def lowest_spending_cateory(self) -> dict[str, int | str]:
-        lowest_category = self.storage.get_lowest_spending_category()
-
-        return {lowest_category[0]: f"{lowest_category[1] / 100:.2f}"}
-
-    def category_spending(self) -> list[dict]:
+    def get_all_category_spending(self, user_id: int) -> list:
         all_spending_categories = []
-        spending_categories = self.storage.get_category_spending()
 
-        for spending_category in spending_categories:
-            normalized_category = {
-                spending_category[0]: f"{spending_category[1] / 100:.2f}"
+        fetched_spendings = self.expense_storage.get_all_category_spending(user_id)
+
+        for spending in fetched_spendings:
+            spending_data = {
+                "category_name": self.category_service.get_category_name(spending[0]),
+                "total": f"{spending[1] / 100:.2f}",
             }
 
-            all_spending_categories.append(normalized_category)
+            all_spending_categories.append(spending_data)
 
         return all_spending_categories
+
+    def highest_spending_category(self, user_id: int) -> list:
+        highest_spendings = []
+
+        spendings = self.expense_storage.get_highest_spending_category(user_id)
+
+        for spending in spendings:
+            spending_data = {
+                "category_name": self.category_service.get_category_name(spending[0]),
+                "total": f"{spending[1] / 100:.2f}",
+            }
+
+            highest_spendings.append(spending_data)
+
+        return highest_spendings
+
+    def lowest_spending_category(self, user_id: int) -> list:
+        lowest_spendings = []
+
+        spendings = self.expense_storage.get_lowest_spending_category(user_id)
+
+        for spending in spendings:
+            spending_data = {
+                "category_name": self.category_service.get_category_name(spending[0]),
+                "total": f"{spending[1] / 100:.2f}",
+            }
+
+            lowest_spendings.append(spending_data)
+
+        return lowest_spendings
+
+    def get_budget_spending(self, user_id: int) -> list:
+        budget_spendings = []
+
+        spendings = self.budget_storage.get_budget_spending(user_id)
+
+        for spending in spendings:
+            spending_data = {
+                "category_id": spending[0],
+                "budget": f"{spending[1] / 100:.2f}",
+                "spending": f"{spending[2] / 100:.2f}",
+                "remaining": f"{spending[3] / 100:.2f}",
+                "flag": spending[4],
+                "month": spending[5],
+                "year": spending[6],
+            }
+
+            budget_spendings.append(spending_data)
+
+        return budget_spendings
